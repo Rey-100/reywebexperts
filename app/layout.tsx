0880d14d0a5408376d8544a3331d7521a1591cbd
Rey-100 <reyvms@yahoo.es>
2025-10-29 20:03:34 -0500

    c6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import './styles.css';
+//import "./globals.css";
 
 // ✅ Línea Nueva (Importar el archivo renombrado)
-import './styles.css';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
