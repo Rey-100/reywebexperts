360f9addfc49335e80e8c86391263c7d1ff15efc
Rey-100 <reyvms@yahoo.es>
2025-11-05 12:05:30 -0500

    cambio 18

--- components/ThreeDMarqueeA.tsx
+++ components/ThreeDMarqueeA.tsx
@@ -1,6 +1,6 @@
 //components\ThreeDMarqueeA.tsx
 
-
+/*  */
 "use client";
 
 
@@ -9,39 +9,30 @@ import { ThreeDMarquee } from "@/components/ui/3d-marquee";
 
 export default function ThreeDMarqueeA() {
   const images = [
-    "https://assets.aceternity.com/cloudinary_bkp/3d-card.png",
-    "https://assets.aceternity.com/animated-modal.png",
-    "https://assets.aceternity.com/animated-testimonials.webp",
-    "https://assets.aceternity.com/cloudinary_bkp/Tooltip_luwy44.png",
-    "https://assets.aceternity.com/github-globe.png",
-    "https://assets.aceternity.com/glare-card.png",
-    "https://assets.aceternity.com/layout-grid.png",
-    "https://assets.aceternity.com/flip-text.png",
-    "https://assets.aceternity.com/hero-highlight.png",
-    "https://assets.aceternity.com/carousel.webp",
-    "https://assets.aceternity.com/placeholders-and-vanish-input.png",
-    "https://assets.aceternity.com/shooting-stars-and-stars-background.png",
-    "https://assets.aceternity.com/signup-form.png",
-    "https://assets.aceternity.com/cloudinary_bkp/stars_sxle3d.png",
-    "https://assets.aceternity.com/spotlight-new.webp",
-    "https://assets.aceternity.com/cloudinary_bkp/Spotlight_ar5jpr.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Parallax_Scroll_pzlatw_anfkh7.png",
-    "https://assets.aceternity.com/tabs.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Tracing_Beam_npujte.png",
-    "https://assets.aceternity.com/cloudinary_bkp/typewriter-effect.png",
-    "https://assets.aceternity.com/glowing-effect.webp",
-    "https://assets.aceternity.com/hover-border-gradient.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Infinite_Moving_Cards_evhzur.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Lamp_hlq3ln.png",
-    "https://assets.aceternity.com/macbook-scroll.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Meteors_fye3ys.png",
-    "https://assets.aceternity.com/cloudinary_bkp/Moving_Border_yn78lv.png",
-    "https://assets.aceternity.com/multi-step-loader.png",
-    "https://assets.aceternity.com/vortex.png",
-    "https://assets.aceternity.com/wobble-card.png",
-    "https://assets.aceternity.com/world-map.webp",
+      "/images/1.webp",
+      "/images/2.webp",
+      "/images/3.webp",
+      "/images/4.webp",
+      "/images/5.webp",
+      "/images/6.webp",
+      "/images/7.webp",
+      "/images/8.webp",
+      "/images/9.webp",
+      "/images/10.webp",
+      "/images/11.webp",
+      "/images/12.webp",
+      "/images/13.webp",
+      "/images/14.webp",
+      "/images/15.webp",
+      "/images/16.webp",
+      "/images/17.webp",
+      "/images/18.webp",
+      "/images/19.webp",
+      "/images/20.webp",
+      "/images/21.webp",
+   
   ];
-
+ 
   return (
     <div className="relative my-10 flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-3xl">
       <h2 className="relative z-20 mx-auto max-w-4xl text-center text-2xl font-bold text-balance text-white md:text-4xl lg:text-6xl">
@@ -67,7 +58,7 @@ export default function ThreeDMarqueeA() {
       </div>
 
       {/* overlay */}
-      <div className="absolute inset-0 z-10 h-full w-full bg-black/20 dark:bg-black/40" />
+      <div className="absolute inset-0 z-10 h-full w-full bg-black/60 dark:bg-black/40" />
 
       {/* Componente visual de las imágenes */}
       <ThreeDMarquee
