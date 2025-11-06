5781790bb7252924f6694fe665356faaf2b4fba1
Rey-100 <reyvms@yahoo.es>
2025-11-05 21:58:02 -0500

    Cambio 20

--- components/ThreeDMarqueeA.tsx
+++ components/ThreeDMarqueeA.tsx
@@ -30,6 +30,26 @@ export default function ThreeDMarqueeA() {
       "/images/19.webp",
       "/images/20.webp",
       "/images/21.webp",
+      "/images/22.webp",
+      "/images/23.webp",
+      "/images/24.webp",
+      "/images/25.webp",
+      "/images/26.webp",
+      "/images/27.webp",
+      "/images/28.webp",
+      "/images/29.webp",
+      "/images/30.webp",
+      "/images/31.webp",
+      "/images/32.webp",
+      "/images/33.webp",
+      "/images/34.webp",
+
+
+
+
+
+
+
    
   ];
  
